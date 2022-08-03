090de3c9ccf3c22d5a3f8970bdb66805b529590b
T34Virus <treindle@gmail.com>
2022-08-02 21:52:32 -0700

    Add: countapi-js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as countapi from 'countapi-js';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teresaN-trial-project';
+  
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
